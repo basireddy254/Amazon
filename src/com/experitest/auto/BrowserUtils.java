2789afe6b3ffc5ab0954127ee9c240eaf8cc0c65
sekar <venkatdevops44@gmail.com>
2019-11-06 21:30:18 +0530

    Appended Assertions and Screenshots code

---
+++ src/com/experitest/auto/BrowserUtils.java
@@ -0,0 +1,21 @@
+package com.experitest.auto;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class BrowserUtils extends BaseTest {
+	static TakesScreenshot ts;
+	static File image;
+	
+	public static void captureScreenshot(WebDriver ob,String imageName) throws IOException {
+		ts = (TakesScreenshot)ob;
+		image= ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(image, new File("screenshots\\"+imageName+".jpg"));
+	}
+
+}
